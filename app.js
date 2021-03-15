0a35b1ed4267b2ac4150d19ab5ff14fa43a6c282
Styven Maldonado <maldonadostyven@gmail.com>
2021-03-15 14:06:51 -0500

    Add @ampproject/toolbox-optimizer layer

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require('express')
+const AmpOptimizer = require('@ampproject/toolbox-optimizer');
 const app = express()
 
-app.get('*', function (req,res,next) {
-    res.send('hello world')
+
+app.get('*', async function (req,res,next) {
+    try {
+        res.setHeader('Content-Type', 'text/html')
+        const ampOptimizer = AmpOptimizer.create();
+        const routes = '<h1>Hello World!!!</h1>'
+        const html = await ampOptimizer.transformHtml(routes)
+        res.status(200).send(html)
+      } catch (error) {
+          console.log(error)
+      }
 })
 
 app.listen(process.env.PORT || 5000)
+
+console.log(`Server run http://localhost:${process.env.PORT || 5000}`)
 
 module.exports = app
