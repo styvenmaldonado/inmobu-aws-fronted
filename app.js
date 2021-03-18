06f45d7c89a4ae0bbf5c42ba10a7ae18ee9d227d
Styven Maldonado <maldonadostyven@gmail.com>
2021-03-17 21:38:37 -0500

    uninstall cors amp-toolbox-cors

--- app.js
+++ app.js
@@ -1,24 +1,14 @@
 const express = require('express')
 const AmpOptimizer = require('@ampproject/toolbox-optimizer');
-const cors = require('cors')
-const ampCors = require('amp-toolbox-cors');
 const compression = require('compression')
 const renderRoutes = require('./src/frontend/renderRoutes')
 
 const app = express()
-
-app.use(cors())
-app.use(ampCors({
-    sourceOriginPattern: /https:\/\/inmobu\.com$/,
-    verifyOrigin: false
-  }))
 app.use(compression())
  
 
 app.get('*', async function (req,res,next) {
     try {
-        res.header("Access-Control-Allow-Origin",req.url); 
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         res.setHeader('Content-Type', 'text/html')
         const ampOptimizer = AmpOptimizer.create()
         const routes = renderRoutes(req)
