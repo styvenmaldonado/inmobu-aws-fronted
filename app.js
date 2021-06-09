d9acba46d7eda63c0b56da51ed671916d3dea577
Styven Maldonado <maldonadostyven@gmail.com>
2021-06-08 19:20:04 -0500

    Adding CORS express

--- app.js
+++ app.js
@@ -41,6 +41,15 @@ if (NODE_ENV === 'development') {
 
 
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Credentials', true)
+  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next();
+});
+
+
 app.get('*', async function (req, res, next) {
   try {
     res.header('Content-Type: text/html')
